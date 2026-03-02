12a70b5b7ff666341a0c3ff07c27f3b9389a7afa
badusha@snippetcommerce360.com <badusha@snippetcommerce360.com>
2026-03-02 11:34:23 +0530

    intial commit

---
+++ routes/subscription.routes.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+
+const subscriptionRoute = Router();
+
+subscriptionRoute.get('/', (req, res) => res.send({title: 'GET all subscriptions'}));
+
+subscriptionRoute.get('/:id', (req, res) => res.send({title: 'GET subscription details'}));
+
+subscriptionRoute.post('/', (req, res) => res.send({title: 'CREATE subscription '}));
+
+subscriptionRoute.put('/:id', (req, res) => res.send({title: 'UPDATE subscription'}));
+
+subscriptionRoute.delete('/:id', (req, res) => res.send({title: 'DELETE subscription'}));
+
+subscriptionRoute.get('/user/:id', (req, res) => res.send({title: 'GET all user subscriptions'}));
+
+subscriptionRoute.put('/:id/cancel', (req, res) => res.send({title: 'CANCEL subscriptions'}));
+
+subscriptionRoute.get('/upcoming-renewals', (req, res) => res.send({title: 'GET upcoming renewals'}));
+
+
+
+export default subscriptionRoute;
