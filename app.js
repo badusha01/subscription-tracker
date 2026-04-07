b7dd8cfd1d151b3f75e56d7fc2575da5a0519353
badusha@snippetcommerce360.com <badusha@snippetcommerce360.com>
2026-04-07 09:37:32 +0530

    Implemented authentication and rate limiting using arcjet

--- app.js
+++ app.js
@@ -8,15 +8,17 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRoute from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware); 
 
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
+app.use('/api/v1/users', userRouter);   
 app.use('/api/v1/subscriptions', subscriptionRoute);
 
 app.use(errorMiddleware);
